c555039f94aad7f971c07c950a69ba50f998e226
Sasha_Sushchits <sushchitskaban@mail.ru>
2022-02-01 19:09:35 +0300

    tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    Hello
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-16 rounded">
+      Button
+    </button>
   </div>
 </template>
 
